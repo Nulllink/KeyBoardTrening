01c329397c6a8fb5bc8e4b36a3a3eb9929c44149
Nulllink <47364651+Nulllink@users.noreply.github.com>
2020-10-20 16:56:29 +0300

    Initial commit

---
+++ KeyBoardTrening1/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace KeyBoardTrening1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.сновоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.стартToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.паузаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(30, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 61);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(30, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 63);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(30, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 63);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(30, 349);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 63);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(30, 443);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(173, 63);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Items.AddRange(new object[] {
+            "Ф -",
+            "Ы -",
+            "В -",
+            "А -",
+            "О -",
+            "Л -",
+            "Д -",
+            "Ж -"});
+            this.listBox1.Location = new System.Drawing.Point(263, 257);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(152, 229);
+            this.listBox1.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сновоToolStripMenuItem,
+            this.стартToolStripMenuItem,
+            this.паузаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(430, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // сновоToolStripMenuItem
+            // 
+            this.сновоToolStripMenuItem.Name = "сновоToolStripMenuItem";
+            this.сновоToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.сновоToolStripMenuItem.Text = "Снова";
+            this.сновоToolStripMenuItem.Click += new System.EventHandler(this.сновоToolStripMenuItem_Click);
+            // 
+            // стартToolStripMenuItem
+            // 
+            this.стартToolStripMenuItem.Name = "стартToolStripMenuItem";
+            this.стартToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.стартToolStripMenuItem.Text = "Старт";
+            this.стартToolStripMenuItem.Click += new System.EventHandler(this.стартToolStripMenuItem_Click);
+            // 
+            // паузаToolStripMenuItem
+            // 
+            this.паузаToolStripMenuItem.Name = "паузаToolStripMenuItem";
+            this.паузаToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.паузаToolStripMenuItem.Text = "Пауза";
+            this.паузаToolStripMenuItem.Click += new System.EventHandler(this.паузаToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(430, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(263, 62);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 189);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(430, 561);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.OrangeRed;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem сновоToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem стартToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem паузаToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ KeyBoardTrening1/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyBoardTrening1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int[] time = new int[2];
+        string[] array = new string[8];
+        int[] err = new int[8];
+
+        private void start()
+        {
+            array[0] = "Ф";
+            array[1] = "Ы";
+            array[2] = "В";
+            array[3] = "А";
+            array[4] = "О";
+            array[5] = "Л";
+            array[6] = "Д";
+            array[7] = "Ж";
+            time[0] = 0;
+            time[1] = 0;
+            Random rnd = new Random();
+            toolStripStatusLabel1.Text = "00:00";
+            label1.Text = array[rnd.Next(0, 7)];
+            label3.Text = array[rnd.Next(0, 7)];
+            label4.Text = array[rnd.Next(0, 7)];
+            label5.Text = array[rnd.Next(0, 7)];
+            textBox1.Text = "";
+            listBox1.Items[0] = "Ф - ";
+            listBox1.Items[1] = "Ы - ";
+            listBox1.Items[2] = "В - ";
+            listBox1.Items[3] = "А - ";
+            listBox1.Items[4] = "О - ";
+            listBox1.Items[5] = "Л - ";
+            listBox1.Items[6] = "Д - ";
+            listBox1.Items[7] = "Ж - ";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            start();
+        }
+
+        private void сновоToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            start();
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time[1]++;
+            if (time[1] == 61)
+            {
+                time[1] = 1;
+                time[0]++;
+            }
+            toolStripStatusLabel1.Text = time[0].ToString() + ":" + time[1].ToString();
+        }
+
+        private void стартToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void паузаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Random rnd = new Random();
+            string key = e.KeyChar.ToString().ToUpper();
+            
+            if (key == label1.Text)
+            {
+                label2.Text = label1.Text;
+                label1.Text = label3.Text;
+                label3.Text = label4.Text;
+                label4.Text = label5.Text;
+                label5.Text = array[rnd.Next(0, 7)];
+            }
+            else if (label1.Text == "Ф")
+            {
+                err[0]++;
+                listBox1.Items[0] = "Ф - " + (err[0]).ToString();
+            }
+            else if (label1.Text == "Ы")
+            {
+                err[1]++;
+                listBox1.Items[1] = "Ы - " + (err[1]).ToString();
+            }
+            else if (label1.Text == "В")
+            {
+                err[2]++;
+                listBox1.Items[2] = "В - " + (err[2]).ToString();
+            }
+            else if (label1.Text == "А")
+            {
+                err[3]++;
+                listBox1.Items[3] = "А - " + (err[3]).ToString();
+            }
+            else if (label1.Text == "О")
+            {
+                err[4]++;
+                listBox1.Items[4] = "О - " + (err[4]).ToString();
+            }
+            else if (label1.Text == "Л")
+            {
+                err[5]++;
+                listBox1.Items[5] = "Л - " + (err[5]).ToString();
+            }
+            else if (label1.Text == "Д")
+            {
+                err[6]++;
+                listBox1.Items[6] = "Д - " + (err[6]).ToString();
+            }
+            else if (label1.Text == "Ж")
+            {
+                err[7]++;
+                listBox1.Items[7] = "Ж - " + (err[7]).ToString();
+            }
+        }
+    }
+}
